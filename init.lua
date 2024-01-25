9d2476f3b23015bd7b2b1956db8f57ddb7e1d139
Ulic-youthlic <Ulic.youthlic@gmail.com>
2024-01-25 23:07:14 +0800

    add some options for nvim

--- init.lua
+++ init.lua
@@ -3,10 +3,10 @@ require("base")
 
 -- lazy.git
 if vim.fn.executable('git') == 1 and vim.version().minor >= 8 then
-	require("lazy-nvim")
-	require("lazy").setup("plugins")
+    require("lazy-nvim")
+    require("lazy").setup("plugins")
 else
-	vim.cmd([[
-		echoerr 'NOT FOUND: git'
-	]])
+    vim.cmd([[
+        echoerr 'NOT FOUND: git'
+    ]])
 end
---
+++ lua/base/options.lua
@@ -0,0 +1,65 @@
+local option = vim.opt
+local buffer = vim.b
+local global = vim.g
+
+------
+
+local get_os = function()
+    return package.config:sub(1, 1) == "\\" and "win" or "unix"
+end
+local undo_dir = vim.fn.stdpath('data') .. [[/undo]]
+if get_os() == "win" then
+    local term_gui_color = true
+elseif os.getenv([[TERM]]) == 'xterm-256color' then
+    local term_gui_color = true
+else
+    local term_gui_color = false
+end
+
+------
+
+global.mapleader = [[ ]]
+buffer.fileencoding = "utf-8"
+option.backspace = { "indent", "eol", "start" }
+option.tabstop = 4
+option.shiftwidth = 4
+option.expandtab = true
+option.shiftround = true
+option.autoindent = true
+option.smartindent = true
+option.nu = true
+option.rnu = true
+option.wildmenu = true
+option.hlsearch = false
+option.ignorecase = true
+option.smartcase = true
+option.completeopt = { "menu", "noselect" }
+option.cursorline = true
+option.termguicolors = term_gui_color
+option.signcolumn = "yes"
+option.autoread = true
+option.title = true
+option.swapfile = false
+option.backup = false
+option.updatetime = 50
+option.mouse = "a"
+option.undofile = true
+option.undodir = vim.fn.expand(undo_dir)
+option.exrc = true
+option.scrolloff = 5
+option.wrap = false
+option.splitright = true
+option.splitbelow = true
+vim.api.nvim_create_autocmd(
+    [[TextYankPost]],
+    {
+        pattern = [[*]],
+        callback = function()
+            vim.highlight.on_yank({
+                timeout = 500,
+                on_macro = true,
+                on_visual = true
+            })
+        end,
+    }
+)
