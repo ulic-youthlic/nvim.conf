e7295fdab0d4e7799d6c7fdc3c1b5892c8fcaa0d
Ulic-youthlic <Ulic.youthlic@gmail.com>
2024-01-25 11:45:41 +0800

    set nu and realtivenumber as true

--- lua/basic.lua
+++ lua/basic.lua
@@ -1,3 +1,8 @@
+local opt = vim.opt
+
+opt.nu = true
+opt.relativenumber = true
+
 vim.cmd([[
 	au TextYankPost * silent! lua vim.highlight.on_yank({timeout=100, on_macro=true, on_visual=true})
 ]])
