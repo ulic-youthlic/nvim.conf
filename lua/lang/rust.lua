36bedaa0aaa9926e08dcc1a287c08a3bd6bf21f7
ulic-youthlic <Ulic.youthlic@gmail.com>
2024-01-26 22:25:42 +0800

    fix rust lsp

--- lua/lang/rust.lua
+++ lua/lang/rust.lua
@@ -2,20 +2,25 @@ local M = {}
 local is_command = require("fn").is_command
 local dependencies = {}
 local lsp = nil
+local setup = function()
+end
 
 if is_command("rustc") then
+    setup = function()
+        require("lspconfig").rust_analyzer.setup {}
+    end
     dependencies = {
         {
-            'mrcjkb/rustaceanvim',
-            version = '^4', -- Recommended
-            ft = { 'rust' },
-        }
+            'vxpm/ferris.nvim',
+            config = function()
+                require("ferris").setup {}
+            end
+        } 
     }
     lsp = "rust_analyzer"
 end
 
-M.setup = function()
-end
+M.setup = setup
 M.dependencies = dependencies
 M.lsp = lsp
 
