9e07971a9005f2c764b87640120537f246a9b895
Ulic-youthlic <Ulic.youthlic@gmail.com>
2024-01-26 10:10:03 +0800

    fix error throwing

--- init.lua
+++ init.lua
@@ -6,7 +6,5 @@ if vim.fn.executable('git') == 1 and vim.version().minor >= 8 then
     require("lazy-nvim")
     require("lazy").setup("plugins")
 else
-    vim.cmd([[
-        echoerr 'NOT FOUND: git'
-    ]])
+    vim.api.nvim_err_writeln([[NOT FOUND: git]])
 end
