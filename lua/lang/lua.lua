211bee329ff86080810a94979709681d03ea8115
Ulic-youthlic <Ulic.youthlic@gmail.com>
2024-01-26 21:16:31 +0800

    fix lua and rust lsp dependencies

--- lua/lang/lua.lua
+++ lua/lang/lua.lua
@@ -2,13 +2,18 @@ local M = {}
 local is_command = require("fn").is_command
 
 local setup = function()
-    if is_command("lua") or is_command("luajit") then
+end
+local lsp = nil
+
+if is_command("lua") or is_command("luajit") then
+    setup = function()
         require("lspconfig").lua_ls.setup {}
     end
+    lsp = "lua_ls"
 end
 
 M.setup = setup
 M.dependencies = {}
-M.lsp = "lua_ls"
+M.lsp = lsp 
 
 return M
--- lua/lang/rust.lua
+++ lua/lang/rust.lua
@@ -1,6 +1,7 @@
 local M = {}
 local is_command = require("fn").is_command
 local dependencies = {}
+local lsp = nil
 
 if is_command("rustc") then
     dependencies = {
@@ -8,11 +9,12 @@ if is_command("rustc") then
         version = '^4', -- Recommended
         ft = { 'rust' },
     }
+    lsp = "rust_analyzer"
 end
 
 M.setup = function()
 end
 M.dependencies = dependencies
-M.lsp = "rust_analyzer"
+M.lsp = lsp
 
 return M
