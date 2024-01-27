41ba60f41da7460b718f3f6fec86ed52c249cfcc
Ulic-youthlic <Ulic.youthlic@gmail.com>
2024-01-27 21:22:32 +0800

    Add neotree to utils

--- lua/plugins/utils/init.lua
+++ lua/plugins/utils/init.lua
@@ -3,5 +3,6 @@ local M = {}
 M[1] = require("plugins.utils.which_key")
 M[2] = require("plugins.utils.telescope")
 M[3] = require("plugins.utils.jk")
+M[4] = require("plugins.utils.neo_tree")
 
 return M
---
+++ lua/plugins/utils/neo_tree.lua
@@ -0,0 +1,9 @@
+local M = {}
+M[1] = "nvim-neo-tree/neo-tree.nvim"
+M.branch = "v3.x"
+M.dependencies = {
+    "MunifTanjim/nui.nvim",
+    "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+}
+
+return M
