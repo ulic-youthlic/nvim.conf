3d0122f249e8dbd6fc1b45b2e18616f447545df0
Ulic-youthlic <Ulic.youthlic@gmail.com>
2024-01-27 22:15:58 +0800

    Add window-picker plugin

--- lua/plugins/utils/init.lua
+++ lua/plugins/utils/init.lua
@@ -4,5 +4,6 @@ M[1] = require("plugins.utils.which_key")
 M[2] = require("plugins.utils.telescope")
 M[3] = require("plugins.utils.jk")
 M[4] = require("plugins.utils.neo_tree")
+M[5] = require("plugins.utils.window_picker")
 
 return M
