fb5af06ad3bf40267effd2373d9456c4c400c1d8
ulic-youthlic <Ulic.youthlic@gmail.com>
2024-01-29 18:40:09 +0800

    setting scrollview diagnostic signs

--- lua/plugins/appearance/scrollview.lua
+++ lua/plugins/appearance/scrollview.lua
@@ -2,11 +2,15 @@ local M = {}
 
 local config = function()
     require("scrollview").setup {
-        excluded_filetypes = {[[neotree]]},
+        excluded_filetypes = { [[neotree]] },
         current_only = true,
         base = [[right]],
-        signs_on_startup = {[[all]]},
+        signs_on_startup = { [[all]] },
     }
+    vim.g.scrollview_diagnostics_error_symbol = "󰅚 "
+    vim.g.scrollview_diagnostics_warn_symbol = "󰀪 "
+    vim.g.scrollview_diagnostics_hint_symbol = "󰌶 "
+    vim.g.scrollview_diagnostics_info_symbol = " "
 end
 
 M[1] = [[dstein64/nvim-scrollview]]
