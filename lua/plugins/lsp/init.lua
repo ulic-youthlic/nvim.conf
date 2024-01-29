d2a7981d4065cb85773cae18634e33008b7b0f26
Ulic-youthlic <Ulic.youthlic@gmail.com>
2024-01-29 20:37:31 +0800

    disable lsp diagnostic signs' text, only highlighting line num

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -11,20 +11,35 @@ lsp.dependencies = {
     require [[plugins.lsp.lspsaga]],
     require [[plugins.lsp.fidget]],
 }
+
 local config = function()
     lang.setup()
-    local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
-    for type, icon in pairs(signs) do
-        local hl = "DiagnosticSign" .. type
-        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-    end
-    vim.diagnostic.config({
+    vim.diagnostic.config {
         virtual_text = false,
-        signs = true,
+        signs = {
+            text = {
+                [vim.diagnostic.severity.ERROR] = "",
+                [vim.diagnostic.severity.WARN] = "",
+                [vim.diagnostic.severity.HINT] = "",
+                [vim.diagnostic.severity.INFO] = "",
+            },
+            numhl = {
+                [vim.diagnostic.severity.ERROR] = "DiagnosticError",
+                [vim.diagnostic.severity.WARN] = "DiagnosticWarn",
+                [vim.diagnostic.severity.HINT] = "DiagnosticHint",
+                [vim.diagnostic.severity.INFO] = "DiagnosticInfo",
+            },
+            linehl = {
+                [vim.diagnostic.severity.ERROR] = "",
+                [vim.diagnostic.severity.WARN] = "",
+                [vim.diagnostic.severity.HINT] = "",
+                [vim.diagnostic.severity.INFO] = "",
+            },
+            severity_sort = true,
+        },
         underline = true,
         update_in_insert = false,
-        severity_sort = false,
-    })
+    }
 end
 lsp.config = config
 lsp.ft = ft
