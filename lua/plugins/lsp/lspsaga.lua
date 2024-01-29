64b5bc28f25ac8725d39d78fc5297335e9c2a2c8
ulic-youthlic <Ulic.youthlic@gmail.com>
2024-01-29 23:37:38 +0800

    auto show hover diagnostics

--- lua/plugins/lsp/lspsaga.lua
+++ lua/plugins/lsp/lspsaga.lua
@@ -1,8 +1,19 @@
 local M = {}
 
+vim.o.updatetime = 250
+local auto_show_line_diagnostics =  function()
+    vim.api.nvim_create_autocmd({'CursorHold', 'CursorHoldI'}, {
+        group = vim.api.nvim_create_augroup("float_diagnostic", {clear = true}),
+        callback = function()
+            vim.cmd [[Lspsaga show_line_diagnostics ++unfocus]]
+        end
+    })
+end
+
 M[1] = 'nvimdev/lspsaga.nvim'
 M.config = function()
     require('lspsaga').setup {}
+    auto_show_line_diagnostics()
 end
 
 return M
