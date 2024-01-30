c6fddd15206bad2a2ba80f55ddc193e73beec481
ulic-youthlic <Ulic.youthlic@gmail.com>
2024-01-30 09:17:10 +0800

    move global setting, `vim.o.updatetime`, into function

--- lua/plugins/lsp/lspsaga.lua
+++ lua/plugins/lsp/lspsaga.lua
@@ -1,19 +1,23 @@
 local M = {}
 
-vim.o.updatetime = 250
-local auto_show_line_diagnostics =  function()
-    vim.api.nvim_create_autocmd({'CursorHold', 'CursorHoldI'}, {
-        group = vim.api.nvim_create_augroup("float_diagnostic", {clear = true}),
+local show_line_diagnostics = function()
+    vim.o.updatetime = 250
+    vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+        group = vim.api.nvim_create_augroup("float_diagnostic", { clear = true }),
         callback = function()
             vim.cmd [[Lspsaga show_line_diagnostics ++unfocus]]
         end
     })
 end
 
+local auto_cmd = function()
+    show_line_diagnostics()
+end
+
 M[1] = 'nvimdev/lspsaga.nvim'
 M.config = function()
     require('lspsaga').setup {}
-    auto_show_line_diagnostics()
+    auto_cmd()
 end
 
 return M
