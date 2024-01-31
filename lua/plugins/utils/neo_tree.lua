5ef54ead83cdc782503a13afe2d65aa626672544
ulic-youthlic <Ulic.youthlic@gmail.com>
2024-01-31 20:29:55 +0800

    Add keymap for neo_tree toggle

--- lua/plugins/utils/neo_tree.lua
+++ lua/plugins/utils/neo_tree.lua
@@ -8,5 +8,15 @@ M.dependencies = {
     "MunifTanjim/nui.nvim",
     "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
 }
+M.keys = {
+    {
+        [[<m-n>]],
+        function()
+            vim.cmd [[Neotree toggle]]
+        end,
+        mode = { [[n]], [[i]], [[v]] },
+        desc = [[Toggle Neotree]]
+    }
+}
 
 return M
