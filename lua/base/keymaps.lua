116c120c6d789b069075b9b1640adfd69f52cf1f
Ulic-youthlic <Ulic.youthlic@gmail.com>
2024-01-31 17:15:11 +0800

    map `;` to `:`

--- lua/base/keymaps.lua
+++ lua/base/keymaps.lua
@@ -1,2 +1,9 @@
 require("base.terminal")
 
+local mappings = {
+    { mode = [[n]], from = [[;]], to = [[:]], opt = { desc = [[toggle to command mode]] } }
+}
+
+for _, mapping in ipairs(mappings) do
+    vim.keymap.set(mapping.mode, mapping.from, mapping.to, mapping.opt)
+end
