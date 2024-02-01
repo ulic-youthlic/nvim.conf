575b03c0de54d9002c2280917fd36ec98e694587
Ulic-youthlic <Ulic.youthlic@gmail.com>
2024-02-01 23:39:13 +0800

    fix mason settings

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -3,7 +3,7 @@ local M = {}
 M[1] = "williamboman/mason.nvim"
 M.config = function()
     require("mason").setup({
-        Path = [[append]]
+        PATH = [[append]]
     })
 end
 M.cmd = [[Mason]]
