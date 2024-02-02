fcaabf85477f0fa631dbeba9ddbc654e1e6a24fa
Ulic-youthlic <Ulic.youthlic@gmail.com>
2024-02-02 11:27:29 +0800

    fix which-key.nvim settings

--- lua/plugins/utils/which_key.lua
+++ lua/plugins/utils/which_key.lua
@@ -2,7 +2,7 @@ if vim.g.vscode then
     return {}
 end
 local M = {}
-local opt = {
+M.opts = {
     plugins = {
         marks = true,     -- shows a list of your marks on ' and `
         registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
@@ -55,16 +55,16 @@ local opt = {
         zindex = 1000,            -- positive value to position WhichKey above other floating windows.
     },
     layout = {
-        height = { min = 4, max = 25 },                                               -- min and max height of the columns
-        width = { min = 20, max = 50 },                                               -- min and max width of the columns
-        spacing = 3,                                                                  -- spacing between columns
-        align = "left",                                                               -- align columns left, center or right
+        height = { min = 4, max = 25 }, -- min and max height of the columns
+        width = { min = 20, max = 50 }, -- min and max width of the columns
+        spacing = 3,                    -- spacing between columns
+        align = "left",                 -- align columns left, center or right
     },
-    ignore_missing = false,                                                           -- enable this to hide mappings for which you didn't specify a label
+    ignore_missing = false,             -- enable this to hide mappings for which you didn't specify a label
     -- hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "^:", "^ ", "^call ", "^lua " }, -- hide mapping boilerplate
-    show_help = true,                                                                 -- show a help message in the command line for using WhichKey
-    show_keys = true,                                                                 -- show the currently pressed key and its label as a message in the command line
-    triggers = "auto",                                                                -- automatically setup triggers
+    show_help = true,                   -- show a help message in the command line for using WhichKey
+    show_keys = true,                   -- show the currently pressed key and its label as a message in the command line
+    triggers = "auto",                  -- automatically setup triggers
     -- triggers = {"<leader>"} -- or specifiy a list manually
     -- list of triggers, where WhichKey should not wait for timeoutlen and show immediately
     triggers_nowait = {
@@ -98,9 +98,6 @@ M.event = [[VeryLazy]]
 M.init = function()
     vim.o.timeout = true
     vim.o.timeoutlen = 300
-end
-M.config = function()
-    require("which-key").setup(opt)
 end
 
 return M
